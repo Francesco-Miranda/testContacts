c284545164b1ccb88840421ea7c9c9f7231aef08
Francesco Miranda <framira2005@gmail.com>
2025-02-03 11:25:57 +0100

    Implement deleteContact function

--- utils.py
+++ utils.py
@@ -7,7 +7,16 @@ def addContact(contactName, contactTel, contactEmail):
     pass
 
 def deleteContact(contactID):
-    pass
+    if not contactsList["contacts"]:
+        return "La lista dei contatti è vuota."
+    
+    for i in range(len(contactsList["contacts"])):
+        if contactsList["contacts"][i]['ID'] == contactID:
+            contattoEliminato = f"{contactsList['contacts'][i].get('Name', 'N/A')}: {contactsList['contacts'][i].get('Tel', 'N/A')}"
+            contactsList["contacts"].pop(i)
+            return f"Contatto [{contattoEliminato}] eliminato con successo!"
+        
+    return f"Contatto {contactID} non trovato"
 
 def findContact(contactName):
     pass
