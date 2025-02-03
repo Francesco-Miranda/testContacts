9164c68d5a37a00f93d88d1af9494f88962fdf93
GiacomoMarconi <ggiacomo.marconi@gmail.com>
2025-02-03 11:26:08 +0100

    implement findContact funz

--- utils.py
+++ utils.py
@@ -10,4 +10,28 @@ def deleteContact(contactID):
     pass
 
 def findContact(contactName):
-    pass
+    find = False
+    
+    print("-" * 20)
+    
+    for contact in contactsList["contacts"]:
+        if (contact["Name"] == contactName):
+            showContactFromId(contact["ID"])
+            find = True
+    
+    if(not find):
+        print("Contatto non trovato")
+        print("-" * 20)
+
+def showContactFromId(id):
+    for contact in contactsList["contacts"]:
+        if(contact["ID"] == id):
+            print(f"ID: {contact.get('ID', 'N/A')}")
+            print(f"Nome: {contact.get('Name', 'N/A')}")
+            print(f"Telefono: {contact.get('Tel', 'N/A')}")
+            print(f"Email: {contact.get('Email', 'N/A')}")
+            print("-" * 20)
+            return
+        
+    print("Contatto non trovato")
+    print("-" * 20)
